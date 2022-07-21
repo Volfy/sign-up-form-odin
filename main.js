b3e4e5d3b5a4803a7433b1a1bc627db75e3c4ccb
Sasha <volfym79@gmail.com>
2022-07-21 18:38:54 +0200

    add confirm password validation

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 // validate that password and confirm password match before submitting
+
+const warnings = document.querySelectorAll('.warning-pwd-nomatch');
+const warning_invalid = document.querySelector('.warning-pwd-invalid')
+const password = document.querySelector("#password");
+const confirm_password = document.querySelector("#confirm-password");
+
+function check () {
+    if (password.value != confirm_password.value) {
+        warnings.forEach(item => item.style.cssText ="display: block;")
+    }else {
+        warnings.forEach(item => item.style.cssText ="visibility: hidden;")
+    }}
+// will display even if user has not finished entering a password, which is annoying
+// will not show an X when the passwords dont match
+
+
+password.addEventListener('keyup', check);
+confirm_password.addEventListener('keyup', check);
